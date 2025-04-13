26e769a14a52acd92ce9491c715e8e992bafac58
AjayKumarSingh12113 <ajaykushwaha12113@gmail.com>
2025-04-14 02:07:05 +0530

    Implement divide and conquer algorithm

---
+++ DivideAndConquer/MergeSort.java
@@ -0,0 +1,58 @@
+package DivideAndConquer;
+
+public class MergeSort {// time complexity is O(nlogn) but SC - O(N) - which is not good thatswhay we
+                        // use quicksort
+    public static void mergesort(int arr[], int si, int ei) {
+        // base case
+        if (si >= ei) {
+            return;
+        }
+        // kaam
+        int mid = si + (ei - si) / 2;
+        mergesort(arr, si, mid);
+        mergesort(arr, mid + 1, ei);
+
+        // merging
+        merge(arr, si, mid, ei);
+    }
+
+    public static void merge(int arr[], int si, int mid, int ei) {
+        int i = si;
+        int j = mid + 1;
+        int k = 0;
+        int temp[] = new int[ei - si + 1];
+        // both the iteration comparing
+        while (i <= mid && j <= ei) {
+            if (arr[i] < arr[j]) {
+                temp[k] = arr[i];
+                i++;
+                k++;
+
+            } else {
+                temp[k] = arr[j];
+                j++;
+                k++;
+            }
+        }
+        // bache huwe element ke liye
+        while (i <= mid) {
+            temp[k++] = arr[i++];
+        }
+        while (j <= ei) {
+            temp[k++] = arr[j++];
+        }
+        // temp array ko arr me copy kr lo
+        for (k = 0, i = si; k < temp.length; k++, i++) {
+            arr[i] = temp[k];
+        }
+    }
+
+    public static void main(String[] args) {
+        int arr[] = { 6, 3, 9, 5, 2, 8 };
+        mergesort(arr, 0, arr.length - 1);
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+
+    }
+}
---
+++ DivideAndConquer/Sortedrotated.java
@@ -0,0 +1,68 @@
+package DivideAndConquer;
+
+public class Sortedrotated {
+    public static int samed(int arr[], int target) {
+        int a = sortedandrotated(arr, target, 0, arr.length - 1);
+        return a;
+        
+    }
+    public static int sortedandrotated(int arr[], int target, int si, int ei) {
+        if (si > ei) {
+            return -1;
+        }
+        int mid = si + (ei - si) / 2;
+        if (arr[mid] == target) {
+            return mid;
+        }
+
+        if (arr[si] <= arr[mid]) {
+            if (arr[si] <= target && target <= arr[mid]) {
+                return sortedandrotated(arr, target, si, mid - 1);
+            } else {
+                return sortedandrotated(arr, target, mid + 1, ei);
+            }
+        } else {
+            if (arr[mid] <= target && target <= arr[ei]) {
+                return sortedandrotated(arr, target, mid + 1, ei);
+            } else {
+                return sortedandrotated(arr, target, si, mid - 1);
+            }
+        }
+    }
+
+    public static int searchmore(int arr[], int target) {
+        int si = 0;
+        int ei = arr.length - 1;
+       
+        while (si <= ei) {
+            int mid = si + (ei - si) / 2;
+            if (arr[mid] == target) {
+                return mid;
+            }
+            if (arr[si] <= arr[mid]) {
+                if (arr[si] <= target && target <= arr[mid]) {
+                    ei = mid - 1;
+
+                } else {
+                    si = mid + 1;
+                }
+            } else {
+                if (arr[mid] <= target && target <= arr[ei]) {
+                    si = mid + 1;
+                } else {
+                    ei = mid - 1;
+                }
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int arr[] = { 4, 5, 6, 7, 0, 1, 2 };
+        int target = 2;
+        System.out.println(sortedandrotated(arr, target, 0, arr.length - 1));
+        System.out.println(searchmore(arr, target));
+        System.out.println(samed(arr, target));
+    }
+    
+}
