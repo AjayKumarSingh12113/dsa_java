db2710d8126a5b51536e7b517f5a826612479825
AjayKumarSingh12113 <ajaykushwaha12113@gmail.com>
2025-04-30 03:40:57 +0530

    adding container with most water in list

--- ArrayList/Container_with_maxwater.java
+++ ArrayList/Container_with_maxwater.java
@@ -1,5 +1,7 @@
 package ArrayList;
 
+import java.util.ArrayList;
+
 public class Container_with_maxwater {
     //brute force
     public static int max_water(int height[]){
@@ -30,10 +32,31 @@ public class Container_with_maxwater {
         }
         return maxw;
     }
+    public static int max_water2(ArrayList<Integer> height){
+        int st = 0, end = height.size()-1;
+        int maxw = 0;
+        while(st <= end){
+            int width = end - st;
+            int ht = Math.min(height.get(st),height.get(end));
+            maxw= Math.max(maxw,width*ht);
+            if(height.get(st)<height.get(end)){
+                st++;
+            }else{
+                end--;
+            }
+        }
+        return maxw;
+    }
 
     public static void main(String[] args) {
         int height[] = {1,8,6,2,5,4,8,3,7};
         System.out.println(max_water(height));
         System.out.println(max_water2(height));
+
+        ArrayList<Integer> list = new ArrayList<>();
+        list.add(1);list.add(8);list.add(6);list.add(2);
+        list.add(5);list.add(4);list.add(8);list.add(3);
+        list.add(7);
+        System.out.println(max_water2(list));
     }
 }
